88c958c777685e7e1347d1cc79288c314a96d4d6
Rushikesh Acharya <rushikeshacharya09@gmail.com>
2024-06-27 21:53:45 +0530

    feat: frontend (WIP)

---
+++ client/app/page.tsx
@@ -0,0 +1,73 @@
+import Image from "next/image";
+import { BsBell, BsBookmark, BsEnvelope, BsTwitter } from "react-icons/bs";
+import { BiHomeCircle, BiHash, BiUser } from "react-icons/bi";
+import FeedCard from "@/components/FeedCard";
+
+interface TwitterSidebarButton {
+  title: string;
+  icon: React.ReactNode;
+}
+
+const sideBarMenuItems: TwitterSidebarButton[] = [
+  {
+    title: "Home",
+    icon: <BiHomeCircle />,
+  },
+  {
+    title: "Explore",
+    icon: <BiHash />,
+  },
+  {
+    title: "Notification",
+    icon: <BsBell />,
+  },
+  {
+    title: "Messages",
+    icon: <BsEnvelope />,
+  },
+  {
+    title: "Bookmarks",
+    icon: <BsBookmark />,
+  },
+  {
+    title: "Profile",
+    icon: <BiUser />,
+  },
+];
+
+export default function Home() {
+  return (
+    <div>
+      <div className="grid grid-cols-12 h-screen w-screen px-56">
+        <div className="col-span-3 pt-8 px-4">
+          <div className="text-4xl h-fit hover:bg-gray-800 rounded-full p-4 cursor-pointer transition-all w-fit">
+            <BsTwitter />
+          </div>
+          <div className="mt-4 text-2xl pr-4">
+            {sideBarMenuItems.map((item) => (
+              <li
+                className="flex justify-start items-center gap-4 hover:bg-gray-800 rounded-full px-5 py-2 w-fit cursor-pointer placeholder-sky-400 mt-2"
+                key={item.title}
+              >
+                <span>{item.icon}</span>
+                <span>{item.title}</span>
+              </li>
+            ))}
+            <div className="mt-5 px-3 ">
+              <button className="bg-[#1d9bf0] p-4 rounded-full w-full text-lg font-semibold">
+                Tweet
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="col-span-5 border-r-[1px] border-l-[1px] border border-gray-600">
+          <FeedCard/>
+          <FeedCard/>
+          <FeedCard/>
+          <FeedCard/> 
+        </div>
+        <div className="col-span-3"></div>
+      </div>
+    </div>
+  );
+}
---
+++ client/components/FeedCard/index.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Image from "next/image";
+import { BiMessageRounded, BiUpload } from "react-icons/bi";
+import { FaRetweet } from "react-icons/fa";
+import { AiOutlineHeart } from "react-icons/ai";
+
+const FeedCard: React.FC = () => {
+  return (
+    <div className="border border-gray-600 p-4 hover:bg-slate-900 transition-all:cursor-pointer border-r-0 border-l-0 border-b-0">
+      <div className="grid grid-cols-12 gap-3">
+        <div className="col-span-1">
+            <Image src="" alt="user-image" height={50} width={50}></Image>
+            
+        </div>
+        <div className="col-span-11">
+            <h5>Rushikesh Acharya</h5>
+            <p>Hijkl sdfkd skldfjalk a;sldkjf asdflkj awe fa;lskdjflaksdj; al;skdjf;l kajsdlfk jkdlfsjalkfjds;la a;lkdjf; la</p>
+            <div className="flex justify-between mt-5 text-xl items-center p-2">
+                <div className="">
+                    <BiMessageRounded/>
+                </div>
+                <div className="">
+                    <FaRetweet/>
+                </div>
+                <div className="">
+                    <AiOutlineHeart/>
+                </div>
+                <div className="">
+                    <BiUpload/>
+                </div>
+            </div>
+        </div> 
+      </div>
+    </div>
+  );
+}; 
+
+export default FeedCard;
