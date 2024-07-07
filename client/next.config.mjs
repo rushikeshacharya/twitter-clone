cf6b9930ebd133e208383f7e6576f8317c4607e2
Rushikesh Acharya <rushikeshacharya09@gmail.com>
2024-07-07 14:48:20 +0530

    Initial template

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ["avatars.githubusercontent.com"]
-    }
+  reactStrictMode: true,
 };
 
 export default nextConfig;
